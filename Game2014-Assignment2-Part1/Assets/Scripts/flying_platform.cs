b8133314a482678ae7611e6e734c409bb8bc8414
abarnez <icecapper@hotmail.com>
2020-11-09 23:36:51 -0500

    Created bobbing platform

---
+++ Game2014-Assignment2-Part1/Assets/Scripts/flying_platform.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class flying_platform : MonoBehaviour
+{
+    public Transform farEnd;
+    private Vector3 frometh;
+    private Vector3 untoeth;
+    private float secondsForOneLength = 5f;
+
+   
+
+    void Start()
+    {
+        frometh = transform.position;
+        untoeth = farEnd.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(frometh, untoeth,
+         Mathf.SmoothStep(0f, 1f,
+          Mathf.PingPong(Time.time / secondsForOneLength, 1f)
+        ));
+    }
+}
