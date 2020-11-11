64c9efdd9c8d76ede66c231a85cfaa8472f1aa5c
abarnez <icecapper@hotmail.com>
2020-11-11 12:55:05 -0500

    Added second platform
    added rotating platform

---
+++ Game2014-Assignment2-Part1/Assets/Scripts/turn_platform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turn_platform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+
+
+    public void Rotate()
+    {
+        if (Time.frameCount % 550 == 0) {
+            transform.Rotate(0.0f, 0.0f, 90.0f);
+                }
+    }
+}
