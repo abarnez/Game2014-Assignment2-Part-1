c097d4e21ec7768e20af674604fd1c9cb802d3c0
abarnez <icecapper@hotmail.com>
2020-11-12 22:21:10 -0500

    Added spike platform

---
+++ Game2014-Assignment2-Part1/Assets/Scripts/spike_platform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spike_platform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+
+
+    public void Rotate()
+    {
+        if (Time.frameCount % 350 == 0)
+        {
+            transform.Rotate(0.0f, 0.0f, 180.0f);
+        }
+    }
+}
