e83e3524089b09336a520e0ba294e8fb49640300
abarnez <icecapper@hotmail.com>
2020-12-03 15:00:33 -0500

    adding enemy and bullet script

---
+++ Game2014-Assignment2-Part1/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public bool canShoot;
+    public GameObject bullet;
+    public Transform firePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canShoot = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canShoot)
+        {
+            Shoot();
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log(other.gameObject.name);
+
+        if (other.tag == "Player")
+        {
+            if (other.transform.position.x > transform.position.x)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            }
+
+            if (other.transform.position.x < transform.position.x)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            }
+            Debug.Log("I see player");
+            canShoot = true;
+        }
+
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        //Debug.Log(other.gameObject.name);
+
+        if (other.tag == "Player")
+        {
+         
+            Debug.Log("I cant see player");
+            canShoot = false;
+        }
+
+    }
+
+    public void Shoot()
+    {
+        if(Time.frameCount % 550 == 0)
+        {
+            Instantiate(bullet, firePoint.transform.position, transform.rotation);
+            bullet.transform.right = transform.right.normalized;
+        }
+    }
+}
---
+++ Game2014-Assignment2-Part1/Assets/Scripts/Enemy_Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Projectile : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        //transform.right += new Vector3(speed, 0.0f);
+        rb.velocity = transform.right * speed;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+
+            Destroy(gameObject);
+        }
+
+    }
+}
